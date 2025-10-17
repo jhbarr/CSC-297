de84af6af070b01f93abba3fff36e9858c989da6
David Rhoades <david@MacBook-Pro-8.local>
2025-10-16 20:43:49 -0500

    Worker threads wokring but needs analysis and comments

--- Worker Threads - NodeJS/Projects/reduce_main.js
+++ Worker Threads - NodeJS/Projects/reduce_main.js
@@ -11,22 +11,21 @@ const fs = require("fs");
 *   - indexStart (int) -> The index that the worker thread should start executing at with respect to the original array
 *   - indexEnd (int) -> The index that the worker thread should stop executing at
 */
-function runWorker(sharedBuffer, resultBuffer, indexChunks)
+function runWorker(sharedBuffer, indexChunks)
 {
     // Create an object to pass to the worker thread
     // This will contain all of the information that the thread needs to execute
     const dataForWorker = {
         sharedBuffer: sharedBuffer,
-        resultBuffer: resultBuffer,
         indexChunks: indexChunks
     };
 
     return new Promise((resolve, reject) => {
-        const worker = new Worker('./filter_2_worker.js', { workerData: dataForWorker });
+        const worker = new Worker('./reduce_worker.js', { workerData: dataForWorker });
 
         // Get the message back from the thread
         worker.on('message', (msg) => {
-            resolve();
+            resolve(msg);
         });    
         worker.on('error', reject);
         worker.on('exit', (code) => {
@@ -47,20 +46,17 @@ function runWorker(sharedBuffer, resultBuffer, indexChunks)
 * OUTPUT
 *   None
 */
-async function parallel_filter(n_workers, arr_len, max_chunk)
+async function parallel_reduce(n_workers, arr_len, max_chunk)
 {
     // Create a shared memory buffer that will be passed to each of the worker threads
     // Wrap an Int32Array around the buffer so that the memory can be more easily modified
     const sharedBuffer = new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT * arr_len);
     const sharedArray = new Int32Array(sharedBuffer); // A typed 32-bit shared integer array buffer
 
-    // Create a shared memory buffer to hold the result of the predicate filter on each of the array elements
-    const filterBuffer = new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT * arr_len);
-    const filterArray = new Int32Array(filterBuffer);
+    // We'll use the shared buffer to hold the original array values; workers will return partial numeric sums
 
-    // Instantiate the values in the shared array
-    for (let i = 1; i <= arr_len; i++) 
-    {
+    // Instantiate the values in the shared array (1..arr_len)
+    for (let i = 1; i <= arr_len; i++) {
         sharedArray[i-1] = i;
     }
 
@@ -78,56 +74,43 @@ async function parallel_filter(n_workers, arr_len, max_chunk)
     }
 
     const worker_chunks = Array.from({ length: n_workers }, () => []);
-        chunks.forEach((range, i) => {
-            worker_chunks[i % n_workers].push(range);
+    chunks.forEach((range, i) => {
+        worker_chunks[i % n_workers].push(range);
     });
 
-    // Instantiate the worker threads
+    // Instantiate the worker threads and collect partial sums
     const workers = [];
-    for (let i = 0; i < n_workers; i++) 
-    {
-        // console.log(worker_chunks[i]);
-        workers.push(runWorker(sharedBuffer, filterBuffer, worker_chunks[i]));
+    for (let i = 0; i < n_workers; i++) {
+        workers.push(runWorker(sharedBuffer, worker_chunks[i]));
     }
 
-    // Wait for all of the worker threads to finish their processes
-    await Promise.all(workers);
+    // Wait for all workers and gather partial results
+    const partials = await Promise.all(workers);
 
-    // Go through each of the values in the results array
-    // If an element in the result array is true, add the element with the corresponding index from the original array to the final array
-    const finalArray = [];
-    for (let i = 0; i < arr_len; i++) 
-    {
-        if (filterArray[i])
-        {
-            finalArray.push(sharedArray[i]);
-        }
-    }
+    // Combine partial sums into final sum
+    const finalSum = partials.reduce((acc, val) => acc + (Number(val) || 0), 0);
 
     // Get the total time of program execution
     const end = performance.now();
     const totalTime = (end - start) / 1000;
 
-    return [Array.from(finalArray), totalTime];
+    return [finalSum, totalTime];
 }
 
 
-/*
-* predicate_func() -> This function takes the sum of all numbers up to the given input and then returns whether that number is even
-* 
-* INPUTS
-*   - x (int) -> The number to be summed to
-* 
-* OUTPUTS
-*   - bool -> Whether the resulting number is even or not
-*/
-function predicate_func(x, y)
-{
-    let sum = 0;
-    for (let i = 0; i < x + y; i++) {
-        sum += 1;
+
+// Simple reduction function (weighted sum with factorial) - keep for serial version if custom logic is desired
+function factorial(n) {
+    if (n === 0 || n === 1) return 1;
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
     }
-    return 10000 + sum % 2 == 0;
+    return result;
+}
+
+function reduce_func(x, y, index) {
+    return x + factorial(y) * index;
 }
 
 /*
@@ -139,49 +122,27 @@ function predicate_func(x, y)
 * OUTPUT
 *   - result_array (Int32Array) -> The resultant array after filtration
 */
-function serial_filter(arr_len)
+function serial_reduce(arr_len)
 {
-    const array = new Array(arr_len)
-    const filterArray = new Array(arr_len);
-    const finalArray = [];
-
-    // Instantiate the values in the shared array
-    for (let i = 1; i <= arr_len; i++) 
-    {
+    const array = new Array(arr_len);
+    for (let i = 1; i <= arr_len; i++) {
         array[i-1] = i;
     }
 
     // Get the current time
     const start = performance.now();
 
-    // Go through the array and set each index to either 0 or 1 in the filter array 
-    // depending on the predicate return of the element from the original array
-    for (let i = 0; i < arr_len; i++)
-    {
-        if (predicate_func(array[i]))
-        {
-            filterArray[i] = 1;
-        }
-        else
-        {
-            filterArray[i] = 0;
-        }
-    }
-
-    // Then go through the filter array and add the element from the corresponding index to the final array
-    for (let i = 0; i < arr_len; i++) 
-    {
-        if (filterArray[i])
-        {
-            finalArray.push(array[i]);
-        }
+    let result = 0;
+    // Perform numeric reduction (sum) by accumulating
+    for (let i = 0; i < array.length; i++) {
+        result = reduce_func(result, array[i], i);
     }
 
     // Get the total time of program execution
     const end = performance.now();
     const totalTime = (end - start) / 1000;
 
-    return [finalArray, totalTime];
+    return [result, totalTime];
 }
 
 /*
@@ -203,26 +164,28 @@ async function runTrials(arr_len, max_chunk)
         console.log("Thread Count =", n_workers);
 
         for (let trial = 1; trial < 4; trial++){
-            const parallel_results = await parallel_filter(n_workers, arr_len, max_chunk);
-            const serial_results = serial_filter(arr_len);
+            const parallel_results = await parallel_reduce(n_workers, arr_len, max_chunk);
+            const serial_results = serial_reduce(arr_len);
 
-            const parallel_array = parallel_results[0];
-            const serial_array = serial_results[0];
+            const parallel_sum = parallel_results[0];
+            const serial_sum = serial_results[0];
 
             const parallel_time = parallel_results[1];
             const serial_time = serial_results[1];
 
+            console.assert((serial_sum == parallel_sum), "The two sums are not equal");
             console.log("Parallel Time:", parallel_time);
             console.log("Serial Time:", serial_time);
 
-            console.assert(JSON.stringify(parallel_array) === JSON.stringify(serial_array), "The two arrays are not equal");
 
             object = {
                 "Thread Count": n_workers,
                 "Trial": trial,
                 "Serial Time": serial_time,
                 "Parallel Time": parallel_time,
-                "Array Size": arr_len
+                "Array Size": arr_len,
+                "Serial Sum": serial_sum,
+                "Parallel Sum": parallel_sum
             }
 
             data.push(object);
@@ -260,10 +223,3 @@ async function executeProgram(filename = "../Data/filter_2_data.csv")
 // Export the data to the CSV
 executeProgram()
 
-// async function func(n_workers, arr_len, max_chunk)
-// {
-//     const res = await parallel_filter(n_workers, arr_len, max_chunk);
-//     console.log(res);
-// }
-
-// func(4, 20, 2);
--- Worker Threads - NodeJS/Projects/reduce_worker.js
+++ Worker Threads - NodeJS/Projects/reduce_worker.js
@@ -1,40 +1,39 @@
-
 // Get the parent port and data from the main file
 const { parentPort, workerData } = require('worker_threads');
 
 // Dereference the information passed to the worker from the parent thread
 const { sharedBuffer, indexChunks } = workerData;
 
-/*
-* predicate_func(x, y) -> Reduce operator for two values
-* INPUTS: x, y (int)
-* OUTPUT: int (reduced value)
-*/
-function predicate_func(x, y) {
-    // Example: sum
-    return x + y;
+
+// Worker function: weighted sum combiner with factorial
+function factorial(n) {
+    if (n === 0 || n === 1) return 1;
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 }
 
+function reduce_func(x, y, index) {
+    return x + factorial(y) * index;
+}
 
-// Worker reduction function: reduce assigned chunks and return result
+// Worker reduction function: sum assigned chunks and return numeric partial sum
 function reduceChunks(arrayBuffer, indexChunks) {
     const array = new Int32Array(arrayBuffer);
-    let partialResult = null;
+    let partialSum = 0;
     for (let j = 0; j < indexChunks.length; j++) {
         const indexStart = indexChunks[j][0];
         const indexEnd = indexChunks[j][1];
         for (let i = indexStart; i < indexEnd; i++) {
-            const val = Atomics.load(array, i);
-            if (partialResult === null) {
-                partialResult = val;
-            } else {
-                partialResult = predicate_func(partialResult, val);
-            }
+            const val = array[i];
+            partialSum = reduce_func(partialSum, val, i);
         }
     }
-    return partialResult;
+    return partialSum;
 }
 
-// Execute reduction and send result to parent
+// Execute reduction and send numeric partial sum to parent
 const result = reduceChunks(sharedBuffer, indexChunks);
 parentPort.postMessage(result);
