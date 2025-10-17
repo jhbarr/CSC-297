58f5164069bf8aa8cae7957f8753dc41de5890f8
jhb77 <josephbarrows03@gmail.com>
2025-10-17 09:06:36 -0500

    Moved files around to better folder names

---
+++ Web Workers/Reduce/reduce_worker.js
@@ -0,0 +1,39 @@
+
+
+// Worker function: weighted sum combiner with factorial
+function factorial(n) {
+    if (n === 0 || n === 1) return 1;
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+function reduce_func(x, y, index) {
+    return x + factorial(y) * index;
+}
+
+// Worker reduction function: sum assigned chunks and return numeric partial sum
+onmessage = function(event) {
+    const data = event.data;
+    const arrayBuffer = data.arrayBuffer;
+    const indexChunks = data.indexChunks;
+
+    const array = new Int32Array(arrayBuffer);
+    let partialSum = 0;
+    for (let j = 0; j < indexChunks.length; j++) {
+        const indexStart = indexChunks[j][0];
+        const indexEnd = indexChunks[j][1];
+        for (let i = indexStart; i < indexEnd; i++) {
+            const val = array[i];
+            partialSum = reduce_func(partialSum, val, i);
+        }
+    }
+    
+    this.postMessage(partialSum);
+}
+
+// // Execute reduction and send numeric partial sum to parent
+// const result = reduceChunks(sharedBuffer, indexChunks);
+// parentPort.postMessage(result);
