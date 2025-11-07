6a6d83396504b7640164335befde8930101c3fe4
David Rhoades <david.isaac.rhoades@gmail.com>
2025-11-06 19:44:45 -0600

    Removed un-needed factorial function from reduce

--- Web Workers/Reduce/reduce_worker.js
+++ Web Workers/Reduce/reduce_worker.js
@@ -1,13 +1,3 @@
-// Worker function: sum with factorial
-function factorial(n) {
-    if (n === 0 || n === 1) return 1;
-    let result = 1;
-    for (let i = 2; i <= n; i++) {
-        result *= i;
-    }
-    return result;
-}
-
 // Sums intergers up to n
 function summation(n) {
     let result = 0;
